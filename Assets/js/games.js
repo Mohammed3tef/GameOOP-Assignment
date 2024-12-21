0cc1164b3a244ad5d3d0c3d2d2f0520421dbf71e
M.Eldegwy <m.eldegwy@gmail.com>
2024-12-21 22:37:12 +0200

    first commit

---
+++ Assets/js/games.js
@@ -0,0 +1,14 @@
+// import {Game} from './details.js';
+
+export class Game {
+    constructor(id, title, thumbnail, short_description, genre, platform) {
+        this.id = id,
+        this.title = title,
+        this.thumbnail = thumbnail,
+        this.short_description = short_description,
+        this.genre = genre,
+        this.platform = platform
+
+    }
+}
+
